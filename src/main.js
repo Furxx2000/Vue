ca4a7d03af0d18441aa5ae261c470244fb9fc7ab
furxx2000 <furxx2000@gmail.com>
2022-02-15 00:33:01 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp, defineAsyncComponent } from 'vue';
 
-import router from './router';
 import store from './store/index';
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 import BaseBadge from './components/UI/BaseBadge.vue';
 import BaseSpinner from './components/UI/BaseSpinner.vue';
+import router from './router';
 // import BaseDialog from './components/UI/BaseDialog.vue';
 
 const BaseDialog = defineAsyncComponent(() =>
